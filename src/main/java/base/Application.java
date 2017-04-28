4e51436db51f9ebf3afb093db249185f5bcd356b
paihsu <phsu01@calpoly.edu>
2017-04-28 15:08:54 -0700

    update=V1

---
+++ src/main/java/base/Application.java
@@ -0,0 +1,16 @@
+package base;
+
+import base.course.CourseRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
+
+}
