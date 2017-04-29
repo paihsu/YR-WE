2f48f8dfc2fe308ca73902c9f49aa9716cde2260
paihsu <phsu01@calpoly.edu>
2017-04-28 21:18:42 -0700

    update weight class

--- src/main/java/base/Weight.java
+++ src/main/java/base/Weight.java
@@ -1,5 +1,32 @@
 package main.java.base;
 
-public class Weight {
 
+public class Weight {
+	
+	private float currentWeight;
+	private String date;
+	
+	public Weight(float currentWeight, String date) {
+		this.currentWeight = currentWeight;
+		this.date = date;
+	}
+	
+	public Weight() {}
+	
+	public float getCurrentWeight() {
+	    return currentWeight;
+	  }
+	  
+	  public void setCurrentWeight(float currentWeight) {
+	    this.currentWeight = currentWeight;
+	  }
+	  
+	  public String getDate() {
+	    return date;
+	  }
+	  
+	  public void setDate(String date) {
+	    this.date = date;
+	  }
+	  
 }
