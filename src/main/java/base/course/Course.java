da2ac60dddd4f33dbcc084a7a4618fcd6bfb1734
paihsu <phsu01@calpoly.edu>
2017-04-28 21:40:38 -0700

    add course back to see issue

---
+++ src/main/java/base/course/Course.java
@@ -0,0 +1,57 @@
+package base.course;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String prefix;
+    private String number;
+    private String title;
+
+    public Course (){};
+
+    public Course(String prefix, String number, String title) {
+        this.prefix = prefix;
+        this.number = number;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/base/course/CourseRepository.java
@@ -0,0 +1,6 @@
+package base.course;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Long> {
+}
