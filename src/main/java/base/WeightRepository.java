99ac4ab74f4b88faa8309efc2e3b8e93adf570c3
paihsu <phsu01@calpoly.edu>
2017-05-12 17:46:53 -0700

    remove: updateWeight, add: save

--- src/main/java/base/WeightRepository.java
+++ src/main/java/base/WeightRepository.java
@@ -1,9 +1,16 @@
 package base;
 
+import java.util.List;
 public interface WeightRepository {
 
 	Weight updateWeight(float weight);
 	Weight updateDare(String date);
+	
+	Iterable<Weight> findAll();
+	Weight save(Weight weight);
+	List<Weight> save(List<Weight> weight);
+	Weight findWeight(Long id);
+	Weight deleteWeight(Long id);
 	
 	
 }
