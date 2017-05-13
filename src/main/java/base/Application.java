d3ff054790ec808e7f4f1c5a6e3e7606757d0388
paihsu <phsu01@calpoly.edu>
2017-05-12 17:38:22 -0700

    make database memory

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -4,14 +4,20 @@ package base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	private Application app;
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
+    
+    @Bean
+    public WeightRepository weightRepository() {
+		return new DataBaseWeightRepository();
+    }
+   
+    
 }
---
+++ src/main/java/base/DataBaseWeightRepository.java
@@ -0,0 +1,18 @@
+package base;
+
+public class DataBaseWeightRepository implements WeightRepository {
+
+	@Override
+	public Weight updateWeight(float weight) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Weight updateDare(String date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
