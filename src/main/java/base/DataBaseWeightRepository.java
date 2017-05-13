b027a541dfc6deeb927c21f2456ebb9aabde8d1a
paihsu <phsu01@calpoly.edu>
2017-05-12 17:48:59 -0700

    make implements update

--- src/main/java/base/DataBaseWeightRepository.java
+++ src/main/java/base/DataBaseWeightRepository.java
@@ -1,18 +1,39 @@
 package base;
 
+import java.util.List;
+
 public class DataBaseWeightRepository implements WeightRepository {
 
 	@Override
-	public Weight updateWeight(float weight) {
+	public Iterable<Weight> findAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Weight updateDare(String date) {
+	public Weight save(Weight weight) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public List<Weight> save(List<Weight> weight) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Weight findWeight(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Weight deleteWeight(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	
 
 }
