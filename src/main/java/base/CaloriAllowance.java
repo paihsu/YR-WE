45f9bafafedcb745bd57c58cd1beacd79684f05e
paihsu <phsu01@calpoly.edu>
2017-05-26 23:03:33 -0700

    calori the days left

--- src/main/java/base/CaloriAllowance.java
+++ src/main/java/base/CaloriAllowance.java
@@ -1,5 +1,23 @@
 package base;
 
-public class CaloriAllowance {
+import java.util.Date;
+import java.util.GregorianCalendar;
 
+import base.Weight;
+
+public class CaloriAllowance {
+	public static void main(String[] av) {
+	    /** The date at the end of the last century */
+		Weight weight = new Weight();
+	    Date d1 = weight.getfinalDate();
+
+	    /** Today's date */
+	    Date today = new Date();
+
+	    // Get msec from each, and subtract.
+	    long diff = today.getTime() - d1.getTime();
+	    long daysLast = diff / (1000 * 60 * 60 * 24);
+	    
+	    // 
+	  }
 }
