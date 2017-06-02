9097925c185a0a51d5fba7379e2f1503df959b48
paihsu <phsu01@calpoly.edu>
2017-06-02 13:26:08 -0700

    remove the daylast for now

--- src/main/java/base/CaloriAllowance.java
+++ src/main/java/base/CaloriAllowance.java
@@ -9,15 +9,15 @@ public class CaloriAllowance {
 	
 	    /** The date at the end of the last century */
 		Weight weight = new Weight();
-	    Date d1 = weight.getfinalDate();
+	    String d1 = weight.getfinalDate();
 
 	    /** Today's date */
 	    Date today = new Date();
-
+/*
 	    // Get msec from each, and subtract.
 	    long diff = today.getTime() - d1.getTime();
 	    long daysLast = diff / (1000 * 60 * 60 * 24);
-
+*/
 	    // make the calori pre day here
 	    
 }
