5dde6033fff4815bca901a21c09eda26c26207b1
paihsu <phsu01@calpoly.edu>
2017-06-12 17:32:59 -0700

    testing bean

--- src/test/java/base/ApplicationTest.java
+++ src/test/java/base/ApplicationTest.java
@@ -9,8 +9,7 @@ public class ApplicationTest {
 	@Test
 	public void testWeightRepository() {
 		 Application context = new Application();
-		 
-		    //assert(context.weightRepository() instanceof WeightRepositoryMemory);	
+		 assert(context.weightRepository() instanceof WeightRepositoryMemory);	
 	}
 	
 }
