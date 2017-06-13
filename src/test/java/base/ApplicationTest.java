6bb794dbe207986404d4a3ac0c4a02fa0df8fed9
paihsu <phsu01@calpoly.edu>
2017-06-12 17:29:21 -0700

    test bean repository

---
+++ src/test/java/base/ApplicationTest.java
@@ -0,0 +1,16 @@
+package base;
+
+import org.junit.Test;
+
+import base.WeightRepository;
+
+public class ApplicationTest {
+
+	@Test
+	public void testWeightRepository() {
+		 Application context = new Application();
+		 
+		    //assert(context.weightRepository() instanceof WeightRepositoryMemory);	
+	}
+	
+}
